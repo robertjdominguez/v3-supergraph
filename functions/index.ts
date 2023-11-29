c62d056fda92aebec9fe39dae5815a44316b0e36
Rob Dominguez <rob.dominguez@hasura.io>
2023-11-28 19:26:18 -0600

    add stripe

--- functions/index.ts
+++ functions/index.ts
@@ -1,67 +1,112 @@
-// Import the required modules from deno-postgres
-import { Client } from 'https://deno.land/x/postgres@v0.17.0/mod.ts';
+import Stripe from 'npm:stripe@^11.16';
 
-// Define the configuration for the PostgreSQL client
-const client = new Client({
-  user: 'read_only',
-  database: 'v3-docs-sample-app',
-  hostname: '35.236.11.122',
-  password: 'kd4555jkfjfkdj39f8f8d9d',
-  port: 5432,
+const apiKey: any = Deno.env.get('STRIPE_SECRET_KEY');
+
+// Initialize the Stripe client
+const stripe = new Stripe(apiKey, {
+  apiVersion: '2022-11-15',
 });
 
 /**
- * Retrieves the five most-recent orders for the given user and, if more than one is a t-shirt, categorizes the user as a "t-shirt lover".
- *
- * @param userId - The user ID for which to retrieve orders.
- * @returns Whether the user is a "t-shirt lover".
+ * Returns a user's information
+ * @param {string} userId - The user's id
+ * @returns {any} customer - The customer object from Stripe
  * @pure This function should only query data without making modifications
  */
-export async function isTShirtLover(userId: any) {
-  // Connect to the database
-  await client.connect();
+export async function getUserInfo(userId: string) {
+  try {
+    // Retrieve all customers from Stripe because we can't search by metadata 👎
+    const customers = await stripe.customers.list();
 
-  // Keep track of the number of t-shirts ordered
-  let tShirtCount = 0;
+    // Filter customers by user_id metadata property
+    const customer = customers.data.find((customer: any) => customer.metadata && customer.metadata.user_id === userId);
 
-  // Query the database for the five most-recent orders for the given user
-  const orders = await client.queryObject(
-    'SELECT product_id FROM orders WHERE user_id = $1 ORDER BY created_at DESC LIMIT 5',
-    [userId]
-  );
-
-  // Let's get the product IDs for the orders
-  const productIds = orders.rows.flat().map((order: any) => order.product_id);
-
-  // Then, use each of those to get the category_id of the product
-  const categories = await client.queryObject('SELECT category_id FROM products WHERE id = ANY($1::uuid[])', [
-    productIds,
-  ]);
-
-  // With the category_id, we can get the name of the category for each product
-  const categoryNames = await client.queryObject('SELECT name FROM categories WHERE id = ANY($1::uuid[])', [
-    categories.rows.flat().map((category: any) => category.category_id),
-  ]);
-
-  // For each category check if it's a t-shirt — if it is, increment tShirtCount
-  categoryNames.rows.flat().forEach((category: any) => {
-    if (category.name == 'T-Shirts') {
-      tShirtCount++;
+    // Handle the possibility
+    if (!customer) {
+      console.error('No customer found with the provided user_id');
+      return null;
     }
-  });
 
-  // Determine whether the user is a "t-shirt lover"
-  const lovesShirts = tShirtCount >= 1;
-  if (lovesShirts) {
-    return `User ${userId} is a T-Shirt lover!`;
-  } else {
-    return `User ${userId} is not a T-Shirt lover!`;
+    return customer;
+  } catch (error) {
+    console.error('Error retrieving user information:', error);
+    return null;
   }
 }
 
-// For testing, calling the function directly
-// async function main() {
-//   console.log(await isTShirtLover('82001336-65b7-11ed-b905-7fa26a16d198'));
-// }
+/**
+ * Returns a checkout session url for a user
+ * @param {string} userId - The user's id
+ * @returns {Promise<string | null>} url - The checkout session url
+ * @pure This function should only query data without making modifications
+ */
+export async function getCheckoutSessionUrl(userId: string, priceId: string) {
+  try {
+    const line_items = [
+      {
+        price: priceId,
+        quantity: 1,
+      },
+    ];
 
-// main();
+    // Create a new checkout session
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items,
+      mode: 'payment',
+      metadata: {
+        userId: userId,
+      },
+      success_url: 'http://localhost/success',
+      cancel_url: 'http://localhost/cancel',
+    });
+
+    // Return the checkout session URL
+    return session.url;
+  } catch (error) {
+    console.error('Error creating checkout session:', error);
+    return null;
+  }
+}
+
+/**
+ * Returns all of a user's payments from Stripe
+ * @param {string} userId - The user's id
+ * @returns {any} payments - The user's payments
+ * @pure This function should only query data without making modifications
+ */
+export async function getCompleteCheckoutSessions(userId: string) {
+  try {
+    // Retrieve all sessions from Stripe because we can't search by metadata 👎
+    const checkoutSessions = await stripe.checkout.sessions.list();
+
+    // Filter sessions by user_id metadata property
+    const userSessions = checkoutSessions.data.filter(
+      (session: any) => session.metadata && session.metadata.userId === userId && session.payment_status === 'paid'
+    );
+
+    // For each of those sessions, let's grab the payment intent and its charges
+    const payments = await Promise.all(
+      userSessions.map(async (session: any) => {
+        const paymentIntent = await stripe.paymentIntents.retrieve(session.payment_intent);
+        const charges = await stripe.charges.list({ payment_intent: session.payment_intent });
+
+        return {
+          paymentIntent,
+          charges,
+        };
+      })
+    );
+
+    // Handle the possibility
+    if (!userSessions) {
+      console.error('No sessions found with the provided user_id');
+      return null;
+    }
+
+    return payments;
+  } catch (error) {
+    console.error('Error retrieving payments:', error);
+    return null;
+  }
+}
